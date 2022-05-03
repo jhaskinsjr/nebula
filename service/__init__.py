7c4931d0a3c4c865521e90f74dd383fd32d3f770
John William Haskins Jr <john@hibiscus.local>
2022-05-03 18:08:24 -0400

    grown maximum message size: 1024 -> 2048

--- service/__init__.py
+++ service/__init__.py
@@ -2,6 +2,7 @@ import socket
 import json
 
 class Service:
+    MESSAGE_SIZE = 2048
     def __init__(self, name, host, port):
         self.name = name
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -12,16 +13,16 @@ class Service:
     def __del__(self):
         self.s.close()
     def rx(self):
-        return json.loads(self.s.recv(1024).decode('ascii'))
+        return json.loads(self.s.recv(self.MESSAGE_SIZE).decode('ascii'))
     def tx(self, msg):
-        # HACK: This pads all messages to be exactly 1024 bytes.
+        # HACK: This pads all messages to be exactly self.MESSAGE_SIZE bytes.
         # HACK: It's dumb, but I want to focus on something else right now.
         _message = {
             str: lambda : json.dumps({'text': msg}),
             dict: lambda : json.dumps(msg),
         }.get(type(msg), lambda : json.dumps({'error': 'Undeliverable object'}))().encode('ascii')
-        assert 1024 >= len(_message), 'Message too big!'
-        _message += (' ' * (1024 - len(_message))).encode('ascii')
+        assert self.MESSAGE_SIZE >= len(_message), 'Message too big!'
+        _message += (' ' * (self.MESSAGE_SIZE - len(_message))).encode('ascii')
         self.s.send(_message)
 
 if __name__ == '__main__':
