be4a5047f5735aa7c6c3a64511bc6b1e23d1e582
John William Haskins Jr <john@hibiscus.local>
2022-08-09 20:39:49 -0400

    count speculatively-taken branches and number of correct speculations

--- pipelines/oroblanco/implementation/commit.py
+++ pipelines/oroblanco/implementation/commit.py
@@ -72,7 +72,7 @@ def do_commit(service, state):
                 '%pc': _insn.get('%pc'),
                 'word': _insn.get('word'),
                 'size': _insn.get('size'),
-                **({'speculation_correct': _insn.get('speculative_next_pc') == _insn.get('next_pc')} if 'speculative_next_pc' in _insn.keys() else {}),
+                **({'speculative_next_pc_correct': _insn.get('speculative_next_pc') == _insn.get('next_pc')} if 'speculative_next_pc' in _insn.keys() else {}),
                 **({'speculative_next_pc': _insn.get('speculative_next_pc')} if 'speculative_next_pc' in _insn.keys() else {}),
                 **({'next_pc': _insn.get('next_pc')} if 'next_pc' in _insn.keys() else {}),
                 **({'ret_pc': _insn.get('ret_pc')} if 'ret_pc' in _insn.keys() else {}),
@@ -80,6 +80,10 @@ def do_commit(service, state):
                 **({'result': _insn.get('result')} if 'result' in _insn.keys() else {}),
             },
         }})
+        if _insn.get('speculative_next_pc'):
+            toolbox.report_stats(service, state, 'flat', 'speculative_next_pc')
+            if _insn.get('speculative_next_pc') == _insn.get('next_pc'):
+                toolbox.report_stats(service, state, 'flat', 'speculative_next_pc_correct')
         toolbox.report_stats(service, state, 'flat', 'retires')
     for _insn in _commit: state.get('pending_commit').remove(_insn)
 
