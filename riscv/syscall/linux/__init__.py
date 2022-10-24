efd2adeba2e80e47f834fba5f8712d096587d2ba
John William Haskins Jr <john@hibiscus.local>
2022-10-24 10:22:52 -0400

    first-pass SYS_openat, treat file descriptors as 32-bit signed int

--- riscv/syscall/linux/__init__.py
+++ riscv/syscall/linux/__init__.py
@@ -1,6 +1,7 @@
 # Copyright (C) 2021, 2022 John Haskins Jr.
 
 import os
+import logging
 
 # The syscall numbers were learned from
 # https://github.com/westerndigitalcorporation/RISC-V-Linux/blob/master/riscv-pk/pk/syscall.h
@@ -24,7 +25,7 @@ def do_syscall(syscall_num, a0, a1, a2, a3, a4, a5, **kwargs):
 #         38: do_renameat,
 #         48: do_faccessat,
 #         49: do_chdir,
-#         56: do_openat,
+         56: do_openat,
 #         57: do_close,
 #         62: do_lseek,
 #         63: do_read,
@@ -43,23 +44,53 @@ def do_syscall(syscall_num, a0, a1, a2, a3, a4, a5, **kwargs):
     }.get(int.from_bytes(syscall_num, 'little'), null)(a0, a1, a2, a3, a4, a5, **{**kwargs, **{'syscall_num': int.from_bytes(syscall_num, 'little')}})
 
 def null(a0, a1, a2, a3, a4, a5, **kwargs):
-    print('*** Syscall ({}) not implemented! ***'.format(kwargs.get('syscall_num')))
+    logging.info('*** Syscall ({}) not implemented! ***'.format(kwargs.get('syscall_num')))
     return {
         'done': True,
     }
+def do_openat(a0, a1, a2, a3, a4, a5, **kwargs):
+    if '0' in kwargs.keys():
+        _dir_fd = int.from_bytes(a0[:4], 'little', signed=True)
+        _pathname = kwargs.get('0') + bytes([0])
+        _flags = int.from_bytes(a2, 'little')
+        try:
+            _fd = os.write(_pathname, _flags, dir_fd=_dir_fd)
+        except:
+            logging.info('do_write(): a0        : {}'.format(a0))
+            logging.info('do_write(): a1        : {}'.format(a1))
+            logging.info('do_write(): a2        : {}'.format(a2))
+            logging.info('do_write(): _dir_fd   : {}'.format(_dir_fd))
+            logging.info('do_write(): _pathname : {} ({})'.format(_pathname, len(_pathname)))
+        _retval = {
+            'done': True,
+        }
+    else:
+        _len = 256 # HACK: This is the pathname of SYS_openat, which has
+                   # function prototype openat(int dirfd, const char * pathname, int flags);
+                   # see: https://man7.org/linux/man-pages/man2/open.2.html. How long is
+                   # the pathname? Who knows? It could, in theory, be much more than 256
+                   # bytes, but I can't be bothered with making this general-purpose right now.
+        _retval = {
+            'done': False,
+            'peek': {
+                'addr': int.from_bytes(a1, 'little'),
+                'size': _len,
+            },
+        }
+    return _retval
 def do_write(a0, a1, a2, a3, a4, a5, **kwargs):
     _len = int.from_bytes(a2, 'little')
     if '0' in kwargs.keys():
-        _fd = int.from_bytes(a0, 'little')
+        _fd = int.from_bytes(a0[:4], 'little', signed=True)
         _buf = kwargs.get('0')
         try:
             os.write(_fd, _buf[:_len])
         except:
-            print('do_write(): a0   : {}'.format(a0))
-            print('do_write(): a1   : {}'.format(a1))
-            print('do_write(): a2   : {}'.format(a2))
-            print('do_write(): _fd  : {}'.format(_fd))
-            print('do_write(): _buf : {} ({})'.format(_buf, len(_buf)))
+            logging.info('do_write(): a0   : {}'.format(a0))
+            logging.info('do_write(): a1   : {}'.format(a1))
+            logging.info('do_write(): a2   : {}'.format(a2))
+            logging.info('do_write(): _fd  : {}'.format(_fd))
+            logging.info('do_write(): _buf : {} ({})'.format(_buf, len(_buf)))
         _retval = {
             'done': True,
         }
