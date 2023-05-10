5617aa2b6ae925129780f110ecb9d830401f7a82
John William Haskins Jr <john@hibiscus.local>
2023-05-10 16:31:20 -0400

    components/[regfile|mainmem]/ ; concomitant updates to start.ussim, restore.ussim

---
+++ components/mainmem/__init__.py
@@ -0,0 +1,121 @@
+# Copyright (C) 2021, 2022 John Haskins Jr.
+
+import os
+import sys
+import argparse
+import logging
+import time
+
+import service
+import toolbox
+
+def do_tick(service, state, results, events):
+    for ev in filter(lambda x: x, map(lambda y: y.get('mem'), events)):
+        _cmd = ev.get('cmd')
+        _addr = ev.get('addr')
+        _size = ev.get('size')
+        _data = ev.get('data')
+        if 'poke' == _cmd:
+            poke(state, _addr, _size, _data)
+            toolbox.report_stats(service, state, 'histo', 'poke.size', _size)
+        elif 'peek' == _cmd:
+            service.tx({'result': {
+                'arrival': state.get('config').get('peek_latency_in_cycles') + state.get('cycle'),
+                'mem': {
+                    'addr': _addr,
+                    'size': _size,
+                    'data': peek(state, _addr, _size),
+                }
+            }})
+            toolbox.report_stats(service, state, 'histo', 'peek.size', _size)
+        else:
+            logging.fatal('ev : {}'.format(ev))
+            assert False
+
+def poke(state, addr, size, data):
+    # data : list of unsigned char, e.g., to make an integer, X, into a list
+    # of N little-endian-formatted bytes -> list(X.to_bytes(N, 'little'))
+    _fd = state.get('fd')
+    try:
+        os.lseek(_fd, addr, os.SEEK_SET)
+        os.write(_fd, bytes(data))
+    except:
+        pass
+#    os.write(_fd, data.to_bytes(size, 'little'))
+def peek(state, addr, size):
+    # return : list of unsigned char, e.g., to make an 8-byte quadword from
+    # a list, X, of N bytes -> int.from_bytes(X, 'little')
+    _fd = state.get('fd')
+    try:
+        os.lseek(_fd, addr, os.SEEK_SET)
+        return list(os.read(_fd, size))
+    except:
+        return []
+
+if '__main__' == __name__:
+    parser = argparse.ArgumentParser(description='μService-SIMulator: Main Memory')
+    parser.add_argument('--debug', '-D', dest='debug', action='store_true', help='output debug messages')
+    parser.add_argument('--log', type=str, dest='log', default='/tmp', help='logging output directory (absolute path!)')
+    parser.add_argument('launcher', help='host:port of μService-SIMulator launcher')
+    args = parser.parse_args()
+    assert not os.path.isfile(args.log), '--log must point to directory, not file'
+    while not os.path.isdir(args.log):
+        try:
+            os.makedirs(args.log, exist_ok=True)
+        except:
+            time.sleep(0.1)
+    logging.basicConfig(
+        filename=os.path.join(args.log, '{}.log'.format(os.path.basename(__file__))),
+        format='%(message)s',
+        level=(logging.DEBUG if args.debug else logging.INFO),
+    )
+    logging.debug('args : {}'.format(args))
+    _launcher = {x:y for x, y in zip(['host', 'port'], args.launcher.split(':'))}
+    _launcher['port'] = int(_launcher['port'])
+    logging.debug('_launcher : {}'.format(_launcher))
+    state = {
+        'service': 'mainmem',
+        'cycle': 0,
+        'active': True,
+        'running': False,
+        'ack': True,
+        'fd': None,
+        'config': {
+            'main_memory_filename': '/tmp/mainmem.raw',
+            'main_memory_capacity': 2**32,
+            'peek_latency_in_cycles': 500,
+        },
+    }
+    _service = service.Service(state.get('service'), _launcher.get('host'), _launcher.get('port'))
+    while state.get('active'):
+        state.update({'ack': True})
+        msg = _service.rx()
+#        _service.tx({'info': {'msg': msg, 'msg.size()': len(msg)}})
+#        print('msg : {}'.format(msg))
+        for k, v in msg.items():
+            if {'text': 'bye'} == {k: v}:
+                state.update({'active': False})
+                state.update({'running': False})
+            elif {'text': 'run'} == {k: v}:
+                state.update({'fd': os.open(state.get('config').get('main_memory_filename'), os.O_RDWR|os.O_CREAT)})
+                os.ftruncate(state.get('fd'), state.get('config').get('main_memory_capacity'))
+                state.update({'running': True})
+                state.update({'ack': False})
+            elif 'config' == k:
+                logging.debug('config : {}'.format(v))
+                if state.get('service') != v.get('service'): continue
+                _field = v.get('field')
+                _val = v.get('val')
+                assert _field in state.get('config').keys(), 'No such config field, {}, in service {}!'.format(_field, state.get('service'))
+                state.get('config').update({_field: _val})
+            elif 'tick' == k:
+                state.update({'cycle': v.get('cycle')})
+                _results = v.get('results')
+                _events = v.get('events')
+                do_tick(_service, state, _results, _events)
+            elif 'restore' == k:
+                assert not state.get('running'), 'Attempted restore while running!'
+                state.update({'cycle': v.get('cycle')})
+                _service.tx({'ack': {'cycle': state.get('cycle')}})
+        if state.get('ack') and state.get('running'): _service.tx({'ack': {'cycle': state.get('cycle')}})
+    os.close(state.get('fd'))
---
+++ components/regfile/__init__.py
@@ -0,0 +1,141 @@
+# Copyright (C) 2021, 2022 John Haskins Jr.
+
+import os
+import sys
+import argparse
+import logging
+import time
+
+import service
+import toolbox
+import riscv.constants
+
+def do_tick(service, state, results, events):
+    for ev in filter(lambda x: x, map(lambda y: y.get('register'), events)):
+        _cmd = ev.get('cmd')
+        _name = ev.get('name')
+        _data = ev.get('data')
+        if 'set' == _cmd:
+            assert _name in state.get('registers').keys()
+            assert isinstance(_data, list)
+            if 0 != _name:
+                state.update({'registers': setregister(state.get('registers'), _name, _data)})
+            toolbox.report_stats(service, state, 'histo', 'set.register', _name)
+        elif 'get' == _cmd:
+            assert _name in state.get('registers').keys()
+            service.tx({'result': {
+                'arrival': 1 + state.get('cycle'),
+                'register': {
+                    'name': _name,
+                    'data': getregister(state.get('registers'), _name),
+                }
+            }})
+            toolbox.report_stats(service, state, 'histo', 'get.register', _name)
+        else:
+            logging.fatal('ev   : {}'.format(ev))
+            logging.fatal('_cmd : {}'.format(_cmd))
+            assert False
+
+def setregister(registers, reg, val):
+    return {x: y for x, y in tuple(registers.items()) + ((reg, val),)}
+def getregister(registers, reg):
+    return registers.get(reg, None)
+def snapshot(service, state, addr, mainmem_filename):
+    logging.debug('snapshot({}, {})'.format(addr, mainmem_filename))
+    fd = os.open(mainmem_filename, os.O_RDWR)
+    os.lseek(fd, addr, os.SEEK_SET)
+    for k in ['%pc'] + sorted(filter(lambda x: not '%pc' == x, state.get('registers').keys()), key=str):
+        v = getregister(state.get('registers'), k)
+        os.write(fd, bytes(v))
+        os.lseek(fd, 8, os.SEEK_CUR)
+        service.tx({'info': 'snapshot: {} : {}'.format(k, v)})
+    os.fsync(fd)
+    os.close(fd)
+    toolbox.report_stats(service, state, 'flat', 'snapshot')
+
+if '__main__' == __name__:
+    parser = argparse.ArgumentParser(description='μService-SIMulator: Register File')
+    parser.add_argument('--debug', '-D', dest='debug', action='store_true', help='output debug messages')
+    parser.add_argument('--log', type=str, dest='log', default='/tmp', help='logging output directory (absolute path!)')
+    parser.add_argument('launcher', help='host:port of μService-SIMulator launcher')
+    args = parser.parse_args()
+    assert not os.path.isfile(args.log), '--log must point to directory, not file'
+    while not os.path.isdir(args.log):
+        try:
+            os.makedirs(args.log, exist_ok=True)
+        except:
+            time.sleep(0.1)
+    logging.basicConfig(
+        filename=os.path.join(args.log, '{}.log'.format(os.path.basename(__file__))),
+        format='%(message)s',
+        level=(logging.DEBUG if args.debug else logging.INFO),
+    )
+    logging.debug('args : {}'.format(args))
+    _launcher = {x:y for x, y in zip(['host', 'port'], args.launcher.split(':'))}
+    _launcher['port'] = int(_launcher['port'])
+    logging.debug('_launcher : {}'.format(_launcher))
+    state = {
+        'service': 'regfile',
+        'cycle': 0,
+        'active': True,
+        'running': False,
+        'ack': True,
+        'registers': {
+            **{'%pc': riscv.constants.integer_to_list_of_bytes(0, 64, 'little')},
+            **{x: riscv.constants.integer_to_list_of_bytes(0, 64, 'little') for x in range(32)},
+            **{0x1000_0000 + x: riscv.constants.integer_to_list_of_bytes(0, 64, 'little') for x in range(32)}, # FP registers
+        }
+    }
+    _service = service.Service(state.get('service'), _launcher.get('host'), _launcher.get('port'))
+    while state.get('active'):
+        state.update({'ack': True})
+        msg = _service.rx()
+#        _service.tx({'info': {'msg': msg, 'msg.size()': len(msg)}})
+#        print('msg : {}'.format(msg))
+        for k, v in msg.items():
+            if {'text': 'bye'} == {k: v}:
+                state.update({'active': False})
+                state.update({'running': False})
+            elif {'text': 'run'} == {k: v}:
+                state.update({'running': True})
+                state.update({'ack': False})
+            elif 'tick' == k:
+                state.update({'cycle': v.get('cycle')})
+                if v.get('snapshot'):
+                    _addr = v.get('snapshot').get('addr')
+                    _mainmem_filename = v.get('snapshot').get('mainmem_filename')
+                    snapshot(_service, state, _addr.get('register'), _mainmem_filename)
+                _results = v.get('results')
+                _events = v.get('events')
+                do_tick(_service, state, _results, _events)
+            elif 'restore' == k:
+                assert not state.get('running'), 'Attempted restore while running!'
+                state.update({'cycle': v.get('cycle')})
+                _service.tx({'ack': {'cycle': state.get('cycle')}})
+                _snapshot_filename = v.get('snapshot_filename')
+                _addr = v.get('addr')
+                fd = os.open(_snapshot_filename, os.O_RDWR)
+                os.lseek(fd, _addr.get('register'), os.SEEK_SET)
+                for k in ['%pc'] + sorted(filter(lambda x: not '%pc' == x, state.get('registers').keys()), key=str):
+                    v = list(os.read(fd, 8))
+                    os.lseek(fd, 8, os.SEEK_CUR)
+                    state.update({'registers': setregister(state.get('registers'), k, v)})
+                    _service.tx({'info': 'restore: {} : {}'.format(k, v)})
+                os.close(fd)
+                toolbox.report_stats(_service, state, 'flat', 'restore')
+                _service.tx({'register': {
+                    'cmd': 'set',
+                    'name': '%pc',
+                    'data': getregister(state.get('registers'), '%pc'),
+                }})
+            elif 'register' == k:
+                _cmd = v.get('cmd')
+                _name = v.get('name')
+                if 'set' == _cmd:
+                    state.update({'registers': setregister(state.get('registers'), _name, v.get('data'))})
+                elif 'get' == _cmd:
+                    _ret = getregister(state.get('registers'), _name)
+                    _service.tx({'result': {'register': _ret}})
+        if state.get('ack') and state.get('running'): _service.tx({'ack': {'cycle': state.get('cycle')}})
+    for k, v in state.get('registers').items():
+        logging.info('register {:2} : {}'.format(k, v))
