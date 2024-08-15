254525c0ebcbdd3066b1cabaca51a8460bde3f99
John William Haskins Jr <john@hibiscus.local>
2024-08-14 20:07:03 -0400

    allow multiple outstanding fetches in SharedCache

--- components/sharedcache/__init__.py
+++ components/sharedcache/__init__.py
@@ -4,6 +4,7 @@ import os
 import sys
 import argparse
 import logging
+import functools
 import time
 
 import service
@@ -11,7 +12,7 @@ import toolbox
 import toolbox.stats
 import simplecache
 
-def fetch_block(service, state, coreid, addr, physical):
+def fetch_block_0(service, state, coreid, addr, physical):
     _blockaddr = state.get('cache').blockaddr(addr)
     _blocksize = state.get('cache').nbytesperblock
 #    state.get('pending_fetch').append(_blockaddr)
@@ -28,7 +29,7 @@ def fetch_block(service, state, coreid, addr, physical):
     }})
 #    toolbox.report_stats(service, state, 'flat', '{}_misses'.format(state.get('service')))
     state.get('stats').refresh('flat', '{}_misses'.format(state.get('service')))
-def do_cache(service, state, coreid, addr, size, physical, data=None):
+def do_cache_0(service, state, coreid, addr, size, physical, data=None):
     service.tx({'info': 'addr : {}'.format(addr)})
     _ante = None
     _post = None
@@ -105,8 +106,7 @@ def do_cache(service, state, coreid, addr, size, physical, data=None):
     if len(state.get('pending_fetch')): state.get('pending_fetch').pop(0)
 #    toolbox.report_stats(service, state, 'flat', '{}_accesses'.format(state.get('service')))
     state.get('stats').refresh('flat', '{}_accesses'.format(state.get('service')))
-
-def do_tick(service, state, results, events):
+def do_tick_0(service, state, results, events):
     for _coreid, rs in map(lambda y: (y.get('coreid'), y.get(state.get('next'))), filter(lambda x: x.get(state.get('next')), results)):
         service.tx({'info': 'rs : {} (_coreid : {})'.format(rs, _coreid)})
         service.tx({'info': 'state.pending_fetch : {}'.format(state.get('pending_fetch'))})
@@ -161,6 +161,136 @@ def do_tick(service, state, results, events):
         _data = _op.get('data')
         do_cache(service, state, _coreid, _addr, _size, _physical, _data)
 
+
+
+
+def fetch_block(service, state, op):
+#    toolbox.report_stats(service, state, 'flat', '{}_misses'.format(state.get('service')))
+    state.get('stats').refresh('flat', '{}_misses'.format(state.get('service')))
+    _blockaddr = state.get('cache').blockaddr(op.get('addr'))
+    _blocksize = state.get('cache').nbytesperblock
+    service.tx({'event': {
+        'arrival': 1 + state.get('cycle'),
+        'coreid': op.get('coreid'),
+        state.get('next'): {
+            'cmd': 'peek',
+            'addr': _blockaddr,
+            'size': _blocksize,
+            **({'physical': True} if op.get('physical') else {}),
+        },
+    }})
+def do_cache(service, state, op):
+    _data = state.get('cache').peek(op.get('addr'), op.get('size'))
+    if not _data or state.get('cache').misc(op.get('addr')).get('coreid') != op.get('coreid'):
+        if 'blockaddr' not in op.keys(): fetch_block(service, state, op)
+        return ({**op, **{'blockaddr': state.get('cache').blockaddr(op.get('addr'))}} if 'blockaddr' not in op.keys() else op)
+    if op.get('data'):
+        # POKE
+        state.get('cache').poke(op.get('addr'), op.get('data'))
+        state.get('cache').misc(op.get('addr'), {'coreid': op.get('coreid')})
+        # writethrough
+        service.tx({'event': {
+            'arrival': 1 + state.get('cycle'),
+            'coreid': op.get('coreid'),
+            state.get('next'): {
+                'cmd': 'poke',
+                'addr': op.get('addr'),
+                'size': len(op.get('data')),
+                'data': op.get('data'),
+                **({'physical': True} if op.get('physical') else {}),
+            }
+        }})
+    else:
+        # PEEK
+        pass
+#    toolbox.report_stats(service, state, 'flat', '{}_accesses'.format(state.get('service')))
+    state.get('stats').refresh('flat', '{}_accesses'.format(state.get('service')))
+    return {**op, **{'done': True, 'hit': 'blockaddr' not in op.keys()}}
+
+def do_tick(service, state, results, events):
+    def fusable(a, b): return 'ante' in a.keys() and 'post' in b.keys() and a.get('ante') == b.get('post') and a.get('done') and b.get('done')
+    def fuse(a, b): return {
+        **{k:v for k, v in a.items() if k not in ['ante', 'data', 'hit']},
+        **({'data': a.get('data') + b.get('data')} if 'data' in a.keys() else {}),
+        **{'hit': all(map(lambda x: x.get('hit'), [a, b]))},
+    }
+    for _coreid, rs in map(lambda y: (y.get('coreid'), y.get(state.get('next'))), filter(lambda x: x.get(state.get('next')), results)):
+        service.tx({'info': 'rs : {} (_coreid : {})'.format(rs, _coreid)})
+        service.tx({'info': 'state.executing     : {}'.format(state.get('executing'))})
+        _addr = rs.get('addr')
+        _data = rs.get('data')
+        if not _data: continue
+        if any(map(lambda x: _addr == x.get('blockaddr'), state.get('executing'))):
+#        if any(map(lambda x: _addr == x.get('blockaddr') and _coreid == x.get('coreid'), state.get('executing'))):
+            service.tx({'info': 'rs : {}'.format(rs)})
+            state.get('cache').poke(_addr, _data)
+            state.get('cache').misc(_addr, {'coreid': _coreid})
+    for _perf in map(lambda y: y.get('perf'), filter(lambda x: x.get('perf'), events)):
+        _cmd = _perf.get('cmd')
+        if 'report_stats' == _cmd:
+            _dict = state.get('stats').get(state.get('coreid', -1)).get(state.get('service'))
+            toolbox.report_stats_from_dict(service, state, _dict)
+    for _coreid, ev in map(lambda y: (y.get('coreid'), y.get(state.get('service'))), filter(lambda x: x.get(state.get('service')), events)):
+        ev = {**ev, **{'coreid': _coreid}}
+        service.tx({'info': 'ev : {} (_coreid : {})'.format(ev, _coreid)})
+        if 'cmd' in ev.keys():
+            if 'purge' == ev.get('cmd'):
+                state.get('cache').purge()
+                service.tx({'event': {
+                    'arrival': 1 + state.get('cycle'),
+                    'coreid': _coreid,
+                    state.get('next'): {
+                        'cmd': 'purge',
+                    },
+                }})
+                continue
+            if 'invalidate' == ev.get('cmd'):
+                state.get('cache').invalidate(**{'misc': {'coreid': _coreid}})
+                service.tx({'event': {
+                    'arrival': 1 + state.get('cycle'),
+                    'coreid': _coreid,
+                    state.get('next'): {
+                        'cmd': 'invalidate',
+                    },
+                }})
+                continue
+        if state.get('cache').fits(ev.get('addr'), ev.get('size')):
+            state.get('executing').append(ev)
+        else:
+            _ante = {k:v for k, v in ev.items()}
+            _post = {k:v for k, v in ev.items()}
+            _blockaddr = state.get('cache').blockaddr(ev.get('addr'))
+            _blocksize = state.get('cache').nbytesperblock
+            _ante.update({'ante': state.get('antepost')})
+            _ante.update({'size': _blockaddr + _blocksize - ev.get('addr')})
+            if 'data' in ev.keys(): _ante.update({'data': ev.get('data')[:_ante.get('size')]})
+            _post.update({'post': state.get('antepost')})
+            _post.update({'addr': ev.get('addr') + _ante.get('size')})
+            _post.update({'size': ev.get('size') - _ante.get('size')})
+            if 'data' in ev.keys(): _post.update({'data': ev.get('data')[_ante.get('size'):]})
+            state.get('executing').append(_ante)
+            state.get('executing').append(_post)
+            state.update({'antepost': 1 + state.get('antepost')})
+        service.tx({'info': 'state.executing : {}'.format(state.get('executing'))})
+    state.update({'executing': list(map(lambda op: do_cache(service, state, op), state.get('executing')))})
+    state.update({'executing': functools.reduce(
+        lambda a, b: (a[:-1] + [fuse(a[-1], b)] if len(a) and fusable(a[-1], b) else a + [b]),
+        state.get('executing'),
+        [],
+    )})
+    for op in state.get('executing'):
+        if not op.get('done') or any(map(lambda x: x in op.keys(), ['ante', 'post'])): continue
+        service.tx({'result': {
+            'arrival': (state.get('config').get('hitlatency') if 'hit' in op.keys() else 1) + state.get('cycle'),
+            'coreid': op.get('coreid'),
+            state.get('service'): {
+                'addr': op.get('addr'),
+                'size': op.get('size'),
+                **({} if 'data' in op.keys() else {'data': state.get('cache').peek(op.get('addr'), op.get('size'))}) # NOTE: 'data' in op.keys() => POKE, which don't echo 'data' in 'result'
+            },
+        }})
+    state.update({'executing': list(filter(lambda op: not op.get('done'), state.get('executing')))})
+
 if '__main__' == __name__:
     parser = argparse.ArgumentParser(description='Nebula: Shared Cache')
     parser.add_argument('--debug', '-D', dest='debug', action='store_true', help='output debug messages')
@@ -190,11 +320,12 @@ if '__main__' == __name__:
         'cycle': 0,
         'booted': False,
         'cache': None,
-        'pending_fetch': [],
+#        'pending_fetch': [],
         'active': True,
         'running': False,
         'ack': True,
         'executing': [],
+        'antepost': 0,
         'stats': None,
         'next': args.next,
         'cores': (tuple(range(*(lambda a, b: (a, 1+b))(*map(lambda x: int(x), args.cores.split('-')))))),
@@ -219,15 +350,17 @@ if '__main__' == __name__:
             elif 'reset' == k:
                 _coreid = v.get('coreid')
                 state.update({'executing': list(filter(lambda x: _coreid != x.get('coreid'), state.get('executing')))})
-                state.update({'pending_fetch': list(filter(lambda x: _coreid != x.get('coreid'), state.get('pending_fetch')))})
-                if state.get('booted'): state.get('cache').invalidate(**{'misc': {'coreid': _coreid}})
+#                state.update({'pending_fetch': list(filter(lambda x: _coreid != x.get('coreid'), state.get('pending_fetch')))})
+                if state.get('booted'):
+                    state.get('cache').invalidate(**{'misc': {'coreid': _coreid}})
+#                    state.get('cache').purge()
                 logging.info('@{:15} : {}'.format(state.get('cycle'), msg))
             elif {'text': 'run'} == {k: v}:
                 state.update({'running': True})
                 state.update({'ack': False})
                 if not state.get('booted'):
                     state.update({'booted': True})
-                    state.update({'pending_fetch': []})
+#                    state.update({'pending_fetch': []})
                     state.update({'executing': []})
                     state.update({'stats': toolbox.stats.CounterBank(state.get('coreid', -1), state.get('service'))})
                     state.update({'cache': simplecache.SimpleCache(
